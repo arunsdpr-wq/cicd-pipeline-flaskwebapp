a91023023df4a5eacd712099186c4907f8661bba
arunsdpr-wq <arun.sdpr@gmail.com>
2026-04-17 02:53:53 +0530

    Fix case sensitivity in response data check

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ class BasicTests(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Hello, Jenkins! Your CI/CD pipeline is working!.")
+        self.assertEqual(response.data, b"Hello, Jenkins! Your CI/CD pipeline is Working!.")
 
 if __name__ == '__main__':
     unittest.main()
